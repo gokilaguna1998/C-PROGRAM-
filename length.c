531d616d1ca177830f35d844e816cf6de0d2a00a
gokilaguna1998 <36723035+gokilaguna1998@users.noreply.github.com>
2019-02-05 10:48:27 +0530

    Create length.c

---
+++ length.c
@@ -0,0 +1,51 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#define NO_OF_CHARS 256
+ 
+int min(int a, int b);
+ 
+int longestUniqueSubsttr(char *str)
+{
+    int m = strlen(str);
+    int cur_length = 1;  
+    int max_length = 1;  
+    int prev_index;  
+    int i;
+    int *visited = (int *)malloc(sizeof(int)*NO_OF_CHARS);
+  for (i = 0; i < NO_OF_CHARS;  i++)
+        visited[i] = -1;
+ visited[str[0]] = 0;
+  for (i = 1; i < m; i++)
+    {
+        prev_index =  visited[str[i]];
+  if (prev_index == -1 || i - cur_len > prev_index)
+            cur_len++;
+        else
+        {
+            if (cur_length > max_len)
+                max_length = cur_len;
+ 
+            cur_len = i - prev_index;
+        }
+        visited[str[i]] = i;
+    }
+    if (cur_length > max_len)
+        max_length = cur_len;
+    free(visited);
+    return max_len;
+}
+int min(int a, int b)
+{
+    return (a>b)?b:a;
+}
+
+int main()
+{
+    char str[] = "ABDEFGABEF";
+    printf("The input string is %s m", str);
+    int len =  longestUniqueSubsttr(str);
+    printf("The length of the longest non-repeating "
+           "character substring is %d", len);
+    return 0;
+}
