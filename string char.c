3b9a2fc0270688c2729889e9e6746a8faa3980c3
gokilaguna1998 <36723035+gokilaguna1998@users.noreply.github.com>
2019-02-05 10:51:06 +0530

    Create string char.c

---
+++ string char.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    char str[100];
+    scanf("%s",str);
+    if(str[0]=='S'||str[0]=='s')
+    {
+        printf("Yes");
+    }
+    else 
+    {
+        printf("No");
+    }
+}
